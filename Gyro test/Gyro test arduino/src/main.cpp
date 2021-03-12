22146195b4c6407b7b894f1e51a9baabc8fdf119
JB <jakob.baumgartner@hotmail.com>
2021-03-12 16:34:42 +0100

    gyro callib fun

--- Gyro test/Gyro test arduino/src/main.cpp
+++ Gyro test/Gyro test arduino/src/main.cpp
@@ -2,6 +2,7 @@
 #include "Wire.h"
 
 
+
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 
 int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
@@ -29,11 +30,15 @@ char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, r
 	return tmp_str;
 }
 
+int gyroValuesDgps[3];
+int Gyro_raw_error[3];
 
-void setup() {
 
-	Serial.begin(9600);
-	Wire.begin();
+void setGyro () {
+
+    const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+
+    Wire.begin();
 	Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
 	Wire.write(0x6B); // PWR_MGMT_1 register
 	Wire.write(0); // set to zero (wakes up the MPU-6050)
@@ -41,99 +46,108 @@ void setup() {
 
 	Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
 	Wire.write(0x1B); // GYRO_CONFIG register
-	//Wire.requestFrom(MPU_ADDR, 8);
-	// Wire.endTransmission();
-	// Serial.println(gyro_config);
-	// gyro_config = Wire.read();
-	// Serial.println(gyro_config);
-	//gyro_config |= 0b00010000; // change bit to 1000 scale of range
-	// Serial.println(gyro_config);
-	// Wire.write(0x1B); // select regitster GYRO_CONFIG
-	Wire.write(0x10); // send new values
+    Wire.write(0x10); // send new values
 	Wire.endTransmission(true); // end transmission
-
-	Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
-	Wire.write(0x1B); // GYRO_CONFIG register
-	Wire.endTransmission(true); // end transmission
-	Wire.requestFrom(MPU_ADDR, 1, true);
-
-	gyro_config = Wire.read();
-	Serial.print(gyro_config);
 
 }
 
-void loop() {
+void readGyroDgps () {
+    // return dgps gyro [x, y, z] values in deg
 
-	Wire.beginTransmission(MPU_ADDR);
+    const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+
+
+    int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+    int16_t gyro_x_dgps, gyro_y_dgps, gyro_z_dgps;
+    // int16_t Gyro_dgps_error_x = 0, Gyro_dgps_error_y = 0, Gyro_dgps_error_z = 0;
+
+    Wire.beginTransmission(MPU_ADDR);
 	Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
 	Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
 	Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
 	
-	// "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-	accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-	accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-	accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-	temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-	gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+    gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
 	gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
 	gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+
+    	// converting gyro in degrees/s
+
+	gyro_x_dgps = gyro_x/32.8 - Gyro_raw_error[0];
+	gyro_y_dgps = gyro_y/32.8 - Gyro_raw_error[1];
+	gyro_z_dgps = gyro_z/32.8 - Gyro_raw_error[2];
+
+    gyroValuesDgps[0] = gyro_x_dgps;
+    gyroValuesDgps[1] = gyro_y_dgps;
+    gyroValuesDgps[2] = gyro_z_dgps;
+}
+
+
+void calibrateGyroError () {
+
+    const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+
+    int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+    int16_t gyro_x_dgps, gyro_y_dgps, gyro_z_dgps;
+    // int16_t Gyro_dgps_error_x = 0, Gyro_dgps_error_y = 0, Gyro_dgps_error_z = 0;
+
+    float gyro_error_x = 0;
+    float gyro_error_y = 0;
+    float gyro_error_z = 0;
+
+    for (int i = 0; i <1000; i++) {
+
+        Wire.beginTransmission(MPU_ADDR);
+        Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+        Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+        Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+        
+        gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+        gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+        gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+
+        gyro_x_dgps = gyro_x/32.8;
+        gyro_y_dgps = gyro_y/32.8;
+        gyro_z_dgps = gyro_z/32.8;
+
+        gyro_error_x += gyro_x_dgps;
+        gyro_error_y += gyro_y_dgps;
+        gyro_error_z += gyro_z_dgps;
+		//Serial.println(gyro_y_dgps);
+
+    }
+    
+    Gyro_raw_error[0] = gyro_error_x / 1000;
+    Gyro_raw_error[1] = gyro_error_y / 1000;
+    Gyro_raw_error[2] = gyro_error_z / 1000;
+}
+
+
+
+
+
+
+
+
+
+void setup() {
+Serial.begin(9600);
+setGyro();
+calibrateGyroError();
+Serial.println("---------------------");
+Serial.println(Gyro_raw_error[0]);
+Serial.println(Gyro_raw_error[1]);
+Serial.println(Gyro_raw_error[2]);
+Serial.println("---------------------");
+
+Wire.beginTransmission(MPU_ADDR);
+Wire.write(0x1B);
+Wire.endTransmission(true);
+Wire.requestFrom(MPU_ADDR, 1, true);
+Serial.print(Wire.read());
+}
+
+void loop() {
+readGyroDgps();
+//Serial.println(gyroValuesDgps[2]);
 	
-	// print out data
-	// Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
-	// Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
-	// Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
-	// // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
-	// Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
-	// Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
-	// Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
-	// Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
-	// Serial.println("");
-
-
-	// converting gyro in degrees/s
-
-	gyro_x_dgps = gyro_x/32.8 - Gyro_raw_error_x;
-	gyro_y_dgps = gyro_y/32.8- Gyro_raw_error_y;
-	gyro_z_dgps = gyro_z/32.8 - Gyro_raw_error_z;
-
-	// integrating degrees gyro
-
-	elapsedTime = (millis() - timeRead)/1000;
-	timeRead = millis();
-
-	gyro_x_angle = gyro_x_angle + gyro_x_dgps*elapsedTime;
-	gyro_y_angle = gyro_y_angle + gyro_y_dgps*elapsedTime;
-	gyro_z_angle = gyro_z_angle + gyro_z_dgps*elapsedTime;
-
-
-
-	// printing new values
-
-	// Serial.print(" | gX_angle = "); Serial.print(convert_int16_to_str(gyro_x_angle));
-	// Serial.print(" | gY_angle = "); Serial.print(convert_int16_to_str(gyro_y_angle));
-	// Serial.print(" | gZ_angle = "); Serial.print(convert_int16_to_str(gyro_z_angle));
-	// Serial.println("");
-	// Serial.println(elapsedTime);
-	
-	// Serial.print(" | gX_angle dpgs = "); Serial.println(convert_int16_to_str(gyro_x_dgps));
-	// Serial.print(" | gY_angle dpgs = "); Serial.println(convert_int16_to_str(gyro_y_dgps));
-	// Serial.print(" | gZ_angle dpgs = "); Serial.println(convert_int16_to_str(gyro_z_dgps));
-	// Serial.println("");
-	
-
-	// acc angle converter
-
-	 /*---X---*/
-	Acc_angle_x = (atan((accelerometer_y)/sqrt(pow((accelerometer_x),2) + pow((accelerometer_z),2)))*rad_to_deg); // - Acc_angle_error_x;
-	/*---Y---*/
-	Acc_angle_y = (atan(-1*(accelerometer_x)/sqrt(pow((accelerometer_y),2) + pow((accelerometer_z),2)))*rad_to_deg); // - Acc_angle_error_y; 
-	/*---Z---*/
-	// Acc_angle_z = (atan(-1*(Acc_rawX)/sqrt(pow((Acc_rawY),2) + pow((Acc_rawZ),2)))*rad_to_deg) - Acc_angle_error_y; 
-	
-	// Serial.print("\n\n | gX_angle acc = "); Serial.print((Acc_angle_x));
-	// Serial.print(" | gY_angle acc = "); Serial.print((Acc_angle_y));
-
-
-	// delay();
-	// complementary filter
 }
